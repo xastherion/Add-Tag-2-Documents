3213004bfda0d4fa023e3df0c77cfdf8827308c0
Carlos Gonzalez <xastherion@users.noreply.github.com>
2021-11-24 17:43:46 +0100

    Update addtag.sh

--- addtag.sh
+++ addtag.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
 # Add Tag in Documents Ordner
 mkdir ~/Documents-Temp
-rsync -a ~/Documents/ Documents-Temp
+rsync -a ~/Documents/ ~/Documents-Temp/
 mkdir -m 0700 ~/D
-say "add Tag to D in Finder"
-echo "Press ENTER if D had a Tag"
+echo "Please add a Tag to D in Finder and press ENTER if you are ready with the Tag for D to continue"
+say  "Please add a Tag to D in Finder and press ENTER if you are ready with the Tag for D to continue"
 read "ENTER"
 chmod -N ~/Documents
 rm -r ~/Documents 
 mv -v ~/D ~/Documents
 chmod +a "everyone deny delete" ~/Documents
 killall Finder
+rsync -a ~/Documents-Temp/  ~/Documents/
+rm -r ~/Documents-Temp/ 
